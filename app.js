50e27da1a664817f33adb06577aa44c4d3980e84
Tiago Miste <tiagomiste@gmail.com>
2022-10-10 19:57:08 -0300

    actualizando archivos

--- app.js
+++ app.js
@@ -18,4 +18,7 @@ app.get('/register', (req,res)=> {
 
 app.use(express.static('public'))
 
-app.listen(3030, ()=>  console.log('Servidor en servicio: http://localhost:3030/'));
+// app.listen(3030, ()=>  console.log('Servidor en servicio: http://localhost:3030/')); //
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log("Servidor corriendo en el puerto ${port}"));
