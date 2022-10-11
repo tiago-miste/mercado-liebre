eacc72cfd9c5768d3450160cff07ab0dd78e5ee3
Tiago Miste <tiagomiste@gmail.com>
2022-10-10 21:10:03 -0300

    Proyecto finalizado

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const path = require("path")
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'))
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
 
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'))
+    res.sendFile(path.join(__dirname, './views/login.html'))
 
 })
 
 app.get('/register', (req,res)=> {
-    res.sendFile(path.resolve(dirname, './views/register.html'))
+    res.sendFile(path.join(__dirname, './views/register.html'))
 })
 
 app.use(express.static('public'))
@@ -21,4 +21,4 @@ app.use(express.static('public'))
 // app.listen(3030, ()=>  console.log('Servidor en servicio: http://localhost:3030/')); //
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => console.log("Servidor corriendo en el puerto ${port}"));
+app.listen(port, () => console.log("Servidor corriendo en el puerto ${http://localhost:3001}"));
